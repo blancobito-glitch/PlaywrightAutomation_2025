e5ff396bd295d9222209c12d288a34777700e774
blancbito-glitch <blancobito@gmail.com>
2025-12-12 00:16:28 -0500

    hey

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 300000,
+  timeout: 500000,
   testDir: './src',
   /* Run tests in files in parallel */
   fullyParallel: true,
